b23756c224619e5a571d8f72406f3f30cdbeff4b
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-04-04 14:34:05 -0300

    Mapeando as Classes de Dominio

--- src/main/java/com/bookstore/domain/Categoria.java
+++ src/main/java/com/bookstore/domain/Categoria.java
@@ -1,13 +1,25 @@
 package com.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros;
 
 	public Categoria() {
