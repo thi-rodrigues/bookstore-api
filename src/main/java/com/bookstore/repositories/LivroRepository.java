f3e786b25417499a0a99533d8abbe7acec77c16d
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-04-04 16:20:47 -0300

    Instancia de Dominio, configurado banco H2

--- src/main/java/com/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/bookstore/repositories/LivroRepository.java
@@ -3,7 +3,9 @@ package com.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bookstore.domain.Livro;
+
 @Repository
-public interface Livro extends JpaRepository<Livro, Integer> {
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
 }
