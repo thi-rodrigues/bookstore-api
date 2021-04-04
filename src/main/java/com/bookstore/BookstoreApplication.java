2aabbd217ca5a300cdcdb9b65a0371c9986be0ba
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-04-04 17:00:27 -0300

    Perfil de test implementado

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,40 +1,12 @@
 package com.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.bookstore.domain.Categoria;
-import com.bookstore.domain.Livro;
-import com.bookstore.repositories.CategoriaRepository;
-import com.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livro de Ti");
-
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.setLivros(Arrays.asList(l1));
-
-		categoriaRepository.saveAll(Arrays.asList(cat1));
-		livroRepository.saveAll(Arrays.asList(l1));
-
 	}
 }
---
+++ src/main/java/com/bookstore/DBService.java
@@ -0,0 +1,40 @@
+package com.bookstore;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.Categoria;
+import com.bookstore.domain.Livro;
+import com.bookstore.repositories.CategoriaRepository;
+import com.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+
+	public void instanciaBaseDeDados() {
+		
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Ficcção Científica", "Ficcção científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
+
+		cat1.setLivros(Arrays.asList(l1, l2));
+		cat2.setLivros(Arrays.asList(l3, l4, l5));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+}
